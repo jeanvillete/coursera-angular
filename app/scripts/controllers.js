e8d0f34c580ca8e6a552892f246bab798eff1796
Jean Villete <brunopib@gmail.com>
2016-07-17 19:25:32 -0300

    concluding injection of a factory on lecture 1 of module 3

---
+++ app/scripts/controllers.js
@@ -0,0 +1,93 @@
+'use strict';
+
+var app = angular.module( 'confusionApp' );
+
+app.controller( 'MenuController', [ '$scope', 'menuFactory', function( $scope, menuFactory ){
+    $scope.dishes = menuFactory.getDishes();
+    $scope.tab = 1;
+    $scope.select = function( setTab ) {
+        $scope.tab = setTab;
+
+        if ( setTab === 2 ) {
+            $scope.fillText = 'appetizer';
+        } else if ( setTab === 3 ) {
+            $scope.fillText = 'mains';
+        } else if ( setTab === 4 ) {
+            $scope.fillText = 'dessert';
+        } else {
+            $scope.fillText = '';
+        }
+    };
+
+    $scope.isSelected = function( checkTab ) {
+        return $scope.tab === checkTab;
+    };
+    
+    $scope.fillText = '';
+
+    $scope.showDetails = false;
+    $scope.toggleDetails = function() {
+        $scope.showDetails = !$scope.showDetails;
+    };
+}]);
+
+app.controller( 'ContactController', [ '$scope', function( $scope ){
+    $scope._feedback = function() {
+        return {
+            mychannel : "",
+            firstName : "",
+            lastName : "",
+            agree : false,
+            email : ""
+        };
+    }
+
+    $scope.feedback = $scope._feedback();
+
+    var channels = [
+        { value : "tel", label:"Tel." },
+        { value : "Email", label:"Email" }
+    ];
+    $scope.channels = channels;
+
+    $scope.invalidChannelSelection = false;
+}]);
+
+app.controller( 'FeedbackController', [ '$scope', function( $scope ){
+    $scope.sendFeedback = function() {
+        console.log( $scope.feedback );
+        if ( $scope.feedback.agree && ( !$scope.feedback.mychannel || $scope.feedback.mychannel == '' ) ) {
+            $scope.invalidChannelSelection = true;
+            console.log( 'incorrect' );
+        } else {
+            $scope.invalidChannelSelection = false;
+            $scope.feedback = $scope._feedback();
+            $scope.feedbackForm.$setPristine();
+            console.log( 'correct' );
+        }
+    }
+}]);
+
+app.controller( 'DishDetailController', [ '$scope', 'menuFactory', function( $scope, menuFactory ){
+    var dish = menuFactory.getDish( 3 );
+
+    $scope.dish = dish;
+    $scope.sortby = '';
+    
+    var _comment = function() {
+        return {
+            author : '',
+            rating : 5,
+            comment : '',
+            date : new Date()
+        };
+    }
+
+    $scope.comment = _comment();
+
+    $scope.addComment = function() {
+        dish.comments.push( $scope.comment );
+        $scope.comment = _comment();
+        $scope.commentsForm.$setPristine();
+    };
+}]);
